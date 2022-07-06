f098c04a5421f48855d651f04a7eb84edad1a0d4
Zachary <zacharymoore0523@gmail.com>
2022-07-06 19:45:34 -0400

    Tele and auton modes added
    Tele and auton are added to the names of their respective data points on both SQLite and the Strings files.

--- scouting-xmlParser-master/src/main/test.java
+++ scouting-xmlParser-master/src/main/test.java
@@ -20,13 +20,20 @@ public class test {
         If there is a line that the program doesn't like, it will ask you to reenter it, and then move on
         to the next line afterwards. */
 
+        /*runs line by line and when it hits stop, a mode will cause a shift and start shift into a different mode
+        it should run in mode that spits out both auton and tele, then just tele, then just auton
+         */
 
         //initializing the strings
+        int mode = 0;
         String format1 = "";
         String format2 = "";
         String format3 = "";
-        String test = "";
+        String input = "";
         boolean breakPls = false;
+        boolean autonMode = true;
+        boolean teleMode = true;
+
 
         //reading the file
         URL url = main.class.getResource("Read"); //file path
@@ -38,45 +45,60 @@ public class test {
         boolean firstLoop = true;
 
         //loop to get all the inputs
-        while (!test.equals("stop")) {
+        while (teleMode || autonMode) {
             //getting input
             if (!firstLoop) {
                 previousEnterLoc = enterLoc;
             }
+            //grabs the length of a line of input
             enterLoc = str.indexOf("\n", enterLoc + 1);
-            if (enterLoc==-1){
+            if (enterLoc == -1) {
                 break;
             }
-            test = str.substring(previousEnterLoc + 1, enterLoc);
-            test = test.trim();
-            String test1 = test;
-            if (test1.toUpperCase().equals("STOP")){
-                break;
+            //takes a line of input
+            input = str.substring(previousEnterLoc + 1, enterLoc);
+            input = input.trim();
+            String moddedInput = input;
+            if (moddedInput.equalsIgnoreCase("STOP")) {
+                if (mode == 0) {
+                    autonMode = false;
+                    mode++;
+                } else if (mode == 1) {
+                    autonMode = true;
+                    teleMode = false;
+                    mode++;
+                } else if (mode == 2) {
+                    break;
+                }
+
             }
+
             int numOfCommas = 0;
             int commaLoc = 0;
-            while (test1.contains(",")){
-                commaLoc = test1.indexOf(",");
-                test1 = test1.substring(0, commaLoc) + test1.substring(commaLoc + 1);
+            while (moddedInput.contains(",")){
+                commaLoc = moddedInput.indexOf(",");
+                moddedInput = moddedInput.substring(0, commaLoc) + moddedInput.substring(commaLoc + 1);
                 numOfCommas++;
             }
 
             //in case the program doesn't like a certain line, it will ask you to reenter it here
             while (numOfCommas != 2) {
-                System.out.println(test);
+                if (input.equalsIgnoreCase("stop"))
+                    break;
+                System.out.println(input);
                 System.out.println("Invalid data. Input stop to end data input. Input the data to continue.");
-                test = keyboard.nextLine();
-                test = test.trim();
-                test1 = test;
-                if (test1.toUpperCase().equals("STOP")) {
+                input = keyboard.nextLine();
+                input = input.trim();
+                moddedInput = input;
+                if (moddedInput.equalsIgnoreCase("STOP")) {
                     breakPls = true;
                     break;
                 }
                 numOfCommas = 0;
                 commaLoc = 0;
-                while (test1.contains(",")) {
-                    commaLoc = test1.indexOf(",");
-                    test1 = test1.substring(0, commaLoc) + test1.substring(commaLoc + 1);
+                while (moddedInput.contains(",")) {
+                    commaLoc = moddedInput.indexOf(",");
+                    moddedInput = moddedInput.substring(0, commaLoc) + moddedInput.substring(commaLoc + 1);
                     numOfCommas++;
                 }
             }
@@ -85,34 +107,58 @@ public class test {
             }
 
             //deleting whitespace (except for last space if not camelCase)
-            while (test.contains(", ")) {
-                int spaceLoc = test.indexOf(" ");
-                if (test.charAt(spaceLoc - 1) == ',') {
-                    test = test.substring(0, spaceLoc) + test.substring(spaceLoc + 1);
+            while (input.contains(", ")) {
+                int spaceLoc = input.indexOf(" ");
+                if (input.charAt(spaceLoc - 1) == ',') {
+                    input = input.substring(0, spaceLoc) + input.substring(spaceLoc + 1);
                 }
             }
 
             //first comma location
-            int firstCommaLoc = test.indexOf(",");
-            int secondCommaLoc = test.indexOf(",", firstCommaLoc + 1);
+            int firstCommaLoc = input.indexOf(",");
+            int secondCommaLoc = input.indexOf(",", firstCommaLoc + 1);
             int spaceLoc = secondCommaLoc;
 
-            if (test.indexOf(' ', secondCommaLoc + 2)!=-1) {
-                spaceLoc = test.indexOf(" ", secondCommaLoc + 2);
+            if (input.indexOf(' ', secondCommaLoc + 2)!=-1) {
+                spaceLoc = input.indexOf(" ", secondCommaLoc + 2);
             }
+            String tele;
+            String auton;
 
-            //putting the information in string format
-            format1 = format1 + "\n        <item>" + test.substring(0, firstCommaLoc) + " " + (test.substring(firstCommaLoc + 1, secondCommaLoc)).toUpperCase() + "</item>";
-            if (spaceLoc!=secondCommaLoc) {
-                format2 = format2 + "\n    <string name=\"" + test.substring(0, firstCommaLoc) + "\">" + (test.substring(secondCommaLoc + 1, secondCommaLoc + 2)).toUpperCase() + test.substring(secondCommaLoc + 2, spaceLoc) + test.substring(spaceLoc, spaceLoc + 2).toUpperCase() + test.substring(spaceLoc + 2) + "</string>";
-            } else {
-                format2 = format2 + "\n    <string name=\"" + test.substring(0, firstCommaLoc) + "\">" + (test.substring(secondCommaLoc + 1, secondCommaLoc + 2)) + test.substring(secondCommaLoc + 2) + "</string>";
+            String form2SecHalf;
+            if (!input.equalsIgnoreCase("stop")) {
+                tele = "tele" + input.substring(0, firstCommaLoc) + " " + (input.substring(firstCommaLoc + 1, secondCommaLoc)).toUpperCase();
+                auton = "auto" + input.substring(0, firstCommaLoc) + " " + (input.substring(firstCommaLoc + 1, secondCommaLoc)).toUpperCase();
             }
-            format3 = format3 + "\n        <item>@string/" + test.substring(0, firstCommaLoc) + "</item>";
+            else{
+                tele = "";
+                auton = "";
+            }
+            if (teleMode && !input.equalsIgnoreCase("stop"))
+                format1 =  format1 + "\n        <item>" + tele + "</item>";
+            if (autonMode && !input.equalsIgnoreCase("stop"))
+                format1 = format1 + "\n        <item>" + auton + "</item>";
+            //putting the information in string format
+
+            if (spaceLoc!=secondCommaLoc) {
+                form2SecHalf = (input.substring(secondCommaLoc + 1, secondCommaLoc + 2)).toUpperCase() + input.substring(secondCommaLoc + 2, spaceLoc) + input.substring(spaceLoc, spaceLoc + 2).toUpperCase() + input.substring(spaceLoc + 2) + "</string>";
+                if(teleMode)
+                    format2 = format2 + "\n    <string name=\"" + tele + "\">" + form2SecHalf;
+                if(autonMode)
+                    format2 = format2 + "\n    <string name=\"" + auton + "\">" + form2SecHalf;
+            } else if(!input.equalsIgnoreCase("stop")) {
+                form2SecHalf = (input.substring(secondCommaLoc + 1, secondCommaLoc + 2)) + input.substring(secondCommaLoc + 2) + "</string>";
+                if(teleMode)
+                    format2 = format2 + "\n    <string name=\"" + tele + "\">" + form2SecHalf;
+                if(autonMode)
+                    format2 = format2 + "\n    <string name=\"" + auton + "\">" + form2SecHalf;
+            }
+            if(!input.equalsIgnoreCase("stop"))
+                format3 = format3 + "\n        <item>@string/" + input.substring(0, firstCommaLoc) + "</item>";
 
             firstLoop = false;
         }
-
+//ref in code then name displayed for the string name format
         format2 = "<resources>\n" +
                 "    <string name=\"app_name\">ScoutingApp</string>\n" +
                 "    <string name=\"textbox_default\">0</string>\n" +
@@ -141,7 +187,7 @@ public class test {
                 "        <item>@string/matchNumber</item>\n" +
                 "        <item>@string/teamNumber</item>\n" +
                 "        <item>@string/climb</item>\n" +
-                "   </string-array>"  ;
+                "   </string-array>";
 
         format1 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
                 "<resources>\n" +
@@ -155,7 +201,7 @@ public class test {
                 "        <item>matchNumber INTEGER</item>\n" +
                 "        <item>scouterName TEXT</item>\n" +
                 "        <item>teamNumber INTEGER</item>\n" +
-                "        <item>climb TEXT</item>" + format1 + "\n    </string-array>\n" +
+                "        <item>climb TEXT</item>" + format1 + "\n        <item>comments TEXT</item>\n    </string-array>\n" +
                 "</resources>";
 
         //printing those strings to the files (new files outside the "main" folder)
@@ -181,15 +227,14 @@ public class test {
                     name = names.get(i);
                     format2Write.append("        <item>" + name + "</item>\n");
                 }
-            }
-            else {
-                if (name.equalsIgnoreCase("stop"))
+            } else {
+                if (name.equalsIgnoreCase("stop")) {
                     break;
-                format2Write.append("        <item>" + name + "</item>\n" +
-                "   </string-array>"+
-                        "</resources>");
+                }
+                format2Write.append("        <item>" + name + "</item>\n");
             }
         }
+        format2Write.append( "   </string-array> \n" + "</resources>");
         format2Write.close();
     }
 
